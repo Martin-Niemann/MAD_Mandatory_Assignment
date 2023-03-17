07687959754eef2516300666a9a79695f015e7de
Martin-Niemann <95416410+Martin-Niemann@users.noreply.github.com>
2023-03-17 10:40:55 +0100

    Change sellerPhone from being an int into being a string, as this is the correct spec

--- app/src/main/java/com/martinniemann/mandatoryassignment/models/SalesItem.kt
+++ app/src/main/java/com/martinniemann/mandatoryassignment/models/SalesItem.kt
@@ -3,8 +3,8 @@ package com.martinniemann.mandatoryassignment.models
 import java.io.Serializable
 
 data class SalesItem(val id: Int, val description: String, val price: Int, val sellerEmail: String,
-                     val sellerPhone: Int, val time: Long, val pictureUrl: String)
+                     val sellerPhone: String, val time: Long, val pictureUrl: String)
     : Serializable {
-        constructor(description: String, price: Int, sellerEmail: String, sellerPhone: Int, time: Long)
+        constructor(description: String, price: Int, sellerEmail: String, sellerPhone: String, time: Long)
                 : this(0, description, price, sellerEmail, sellerPhone, time, "")
 }

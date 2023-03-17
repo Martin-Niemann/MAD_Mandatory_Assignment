d3e7dc16295bccf23898ca3605458fd528b338fb
Martin-Niemann <95416410+Martin-Niemann@users.noreply.github.com>
2023-03-17 10:41:21 +0100

    Remove unnecessary comment

--- app/src/main/java/com/martinniemann/mandatoryassignment/repository/SalesItemStoreService.kt
+++ app/src/main/java/com/martinniemann/mandatoryassignment/repository/SalesItemStoreService.kt
@@ -5,9 +5,6 @@ import retrofit2.Call
 import retrofit2.http.*
 
 interface SalesItemStoreService {
-    // this is necessary, as maxPrice cannot be null due to it being an integer,
-    // and as maxPrice = 0 returns free items
-    // one could change maxPrice into being a string and enforce this being a number
     @GET("SalesItems")
     fun getAllSalesItems(): Call<List<SalesItem>>
 
